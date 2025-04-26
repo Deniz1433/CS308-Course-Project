f5569bae21180f490372b6cbffc72be20b0cc841
UtkuuUnlu <129547406+UtkuuUnlu@users.noreply.github.com>
2025-04-26 13:22:06 +0300

    Update Orders.js

--- frontend/src/pages/Orders.js
+++ frontend/src/pages/Orders.js
@@ -26,8 +26,8 @@ const Orders = () => {
 
     const fetchOrders = async () => {
       try {
-        const response = await fetch("http://localhost:5000/api/orders", {
-          credentials: "include",
+        const response = await fetch('/api/orders', {
+          credentials: 'include'
         });
 
         if (!response.ok) {
